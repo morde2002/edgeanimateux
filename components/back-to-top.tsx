0a8c024093e1d9f59e7fd9ba291c4298433a827f
morde2002 <mathengemordecai@gmail.com>
2026-01-12 22:55:33 +0300

    back to top

--- components/back-to-top.tsx
+++ components/back-to-top.tsx
@@ -35,7 +35,7 @@ export function BackToTopButton() {
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.8 }}
-          className="fixed bottom-8 right-8 z-50"
+          className="fixed bottom-8 left-8 z-50"
         >
           <Button
             onClick={scrollToTop}
